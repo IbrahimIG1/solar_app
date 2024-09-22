95deebfb6ec6eca63e155784033a38525df80869
Ibrahim Algammal <109023440+IbrahimIG1@users.noreply.github.com>
2024-09-22 18:29:28 +0300

    Revert "sentry track issus  ✅"

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:solar/core/helper/spacing.dart';
 import 'package:solar/core/routing/routes.dart';
 import 'package:solar/core/widgets/widgets/main_image.dart';
@@ -11,7 +10,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    testSentryError();
     List<Map<String, dynamic>> data = [
       {
         'icon': 'assets/images/screen_one/price_offer_icon.png',
@@ -57,18 +55,5 @@ class HomeScreen extends StatelessWidget {
         ],
       )),
     );
-  }
-
-  Future<void> testSentryError() async {
-    try {
-      int? num;
-      int result = num! + 1;
-    } catch (exception, stackTrace) {
-      print('exception');
-      await Sentry.captureException(
-        exception,
-        stackTrace: stackTrace,
-      );
-    }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import firebase_core
-import package_info_plus
-import sentry_flutter
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
